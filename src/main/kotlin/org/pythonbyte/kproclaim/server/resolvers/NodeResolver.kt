9ce69e62cc4dc1386b327bd04ce414340f481e96
Mark Nenadov <mark.nenadov@borderconnect.com>
2020-10-24 06:59:28 -0400

    bump kproclaim to 1.0 (for api v2 support)

--- src/main/kotlin/org/pythonbyte/kproclaim/server/resolvers/NodeResolver.kt
+++ src/main/kotlin/org/pythonbyte/kproclaim/server/resolvers/NodeResolver.kt
@@ -1,15 +1,9 @@
 package org.pythonbyte.kproclaim.server.resolvers
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
-import org.pythonbyte.krux.json.JsonObject
-import khttp.responses.Response
-import org.pythonbyte.kproclaim.sermonaudio.domain.Recording
-import org.pythonbyte.kproclaim.sermonaudio.domain.Speaker
 import org.pythonbyte.kproclaim.sermonaudio.service.NodeServiceImpl
-import org.pythonbyte.kproclaim.sermonaudio.service.transport.SermonAudioHttp
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.net.URLEncoder
 
 @Component
 class NodeResolver : NodeServiceImpl(), GraphQLQueryResolver {
